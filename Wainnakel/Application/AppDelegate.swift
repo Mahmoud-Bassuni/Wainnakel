8e3c1ba340ca9a3795280ac40a016e4601c5a6cb
Mahmoud-Bassuni <32750640+Mahmoud-Bassuni@users.noreply.github.com>
2019-11-19 23:33:19 +0200

    Update AppDelegate.swift

--- Wainnakel/Application/AppDelegate.swift
+++ Wainnakel/Application/AppDelegate.swift
@@ -13,7 +13,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
    let locationManager = CLLocationManager()
        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-           locationManager.requestAlwaysAuthorization()
+        locationManager.requestAlwaysAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startMonitoringSignificantLocationChanges()
            // Override point for customization after application launch.
            return true
        }
